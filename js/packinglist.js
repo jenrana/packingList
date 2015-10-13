69acbccfd8f8f176eb6cc839338013bdae4ac47c
Jennifer Rana <jennifer.rana@gmail.com>
2015-10-13 14:57:58 -0700

    Modified to show Trips first

--- js/packinglist.js
+++ js/packinglist.js
@@ -1,33 +1,31 @@
-// An example Parse.js Backbone application based on the todo app by
-// [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses Parse to persist
-// the todo items and provide user authentication and sessions.
-
 $(function() {
 
   Parse.$ = jQuery;
 
   // Initialize Parse with your Parse application javascript keys
-Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2eEyTNyMpO98KiwzIm7DCWpi");
+    Parse.initialize("iEHnCz6tBbfat40smeJcdlYuPCcPfI0XOk56W36o", "X6XDmXP6ETqpgK1n3yAD9kyDtSmk0xGrFeIITQvC");
 
-  // Todo Model
+    
+   
+  // Trip Model
   // ----------
 
-  // Our basic Todo model has `content`, `order`, and `done` attributes.
-  var Todo = Parse.Object.extend("Todo", {
-    // Default attributes for the todo.
+  // Our basic Trip model has `tripName`, `order`, and `done` attributes.
+  var Trip = Parse.Object.extend("Trips", {
+    // Default attributes for the trip.
     defaults: {
-      content: "empty todo...",
+      content: "empty trip...",
       done: false
     },
 
-    // Ensure that each todo created has `content`.
+    // Ensure that each trip created has `content`.
     initialize: function() {
       if (!this.get("content")) {
         this.set({"content": this.defaults.content});
       }
     },
 
-    // Toggle the `done` state of this todo item.
+    // Toggle the `done` state of this trip item.
     toggle: function() {
       this.save({done: !this.get("done")});
     }
@@ -40,61 +38,61 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
     }
   });
 
-  // Todo Collection
+  // Trip Collection
   // ---------------
 
-  var TodoList = Parse.Collection.extend({
+  var TripList = Parse.Collection.extend({
 
     // Reference to this collection's model.
-    model: Todo,
+    model: Trip,
 
-    // Filter down the list of all todo items that are finished.
+    // Filter down the list of all trips items that are finished.
     done: function() {
-      return this.filter(function(todo){ return todo.get('done'); });
+      return this.filter(function(trip){ return trip.get('done'); });
     },
 
-    // Filter down the list to only todo items that are still not finished.
+    // Filter down the list to only trips items that are still not finished.
     remaining: function() {
       return this.without.apply(this, this.done());
     },
 
-    // We keep the Todos in sequential order, despite being saved by unordered
+    // We keep the Trips in sequential order, despite being saved by unordered
     // GUID in the database. This generates the next order number for new items.
     nextOrder: function() {
       if (!this.length) return 1;
       return this.last().get('order') + 1;
     },
 
-    // Todos are sorted by their original insertion order.
-    comparator: function(todo) {
-      return todo.get('order');
+    // Trips are sorted by their original insertion order.
+    comparator: function(trip) {
+      return trip.get('order');
     }
 
   });
 
-  // Todo Item View
+  // Trip Item View
   // --------------
 
-  // The DOM element for a todo item...
-  var TodoView = Parse.View.extend({
+  // The DOM element for a trip item...
+  var TripView = Parse.View.extend({
 
     //... is a list tag.
     tagName:  "li",
 
     // Cache the template function for a single item.
-    template: _.template($('#item-template').html()),
+    template: _.template($('#trip-template').html()),
 
     // The DOM events specific to an item.
     events: {
       "click .toggle"              : "toggleDone",
-      "dblclick label.todo-content" : "edit",
-      "click .todo-destroy"   : "clear",
+      "dblclick label.trip-content" : "edit",
+      "click .trip-destroy"   : "clear",
       "keypress .edit"      : "updateOnEnter",
       "blur .edit"          : "close"
     },
 
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a Todo and a TodoView in this
+    // The TripView listens for changes to its model, re-rendering. Since there's
+    // a one-to-one correspondence between a Trip and a TripView in this
     // app, we set a direct reference on the model for convenience.
     initialize: function() {
       _.bindAll(this, 'render', 'close', 'remove');
@@ -102,7 +100,7 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
       this.model.bind('destroy', this.remove);
     },
 
-    // Re-render the contents of the todo item.
+    // Re-render the contents of the trip item.
     render: function() {
       $(this.el).html(this.template(this.model.toJSON()));
       this.input = this.$('.edit');
@@ -138,18 +136,18 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
 
   });
 
-  // The Application
+  // The 
   // ---------------
 
   // The main view that lets a user manage their todo items
-  var ManageTodosView = Parse.View.extend({
+  var ManageTripView = Parse.View.extend({
 
     // Our template for the line of statistics at the bottom of the app.
     statsTemplate: _.template($('#stats-template').html()),
 
     // Delegated events for creating new items, and clearing completed ones.
     events: {
-      "keypress #new-todo":  "createOnEnter",
+      "keypress #new-trip":  "createOnEnter",
       "click #clear-completed": "clearCompleted",
       "click #toggle-all": "toggleAllComplete",
       "click .log-out": "logOut",
@@ -158,33 +156,33 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
 
     el: ".content",
 
-    // At initialization we bind to the relevant events on the `Todos`
+    // At initialization we bind to the relevant events on the `Trips`
     // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved to Parse.
+    // loading any preexisting trips that might be saved to Parse.
     initialize: function() {
       var self = this;
 
       _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'toggleAllComplete', 'logOut', 'createOnEnter');
 
-      // Main todo management template
-      this.$el.html(_.template($("#manage-todos-template").html()));
+      // Main trip management template
+      this.$el.html(_.template($("#manage-trips-template").html()));
       
-      this.input = this.$("#new-todo");
+      this.input = this.$("#new-trip");
       this.allCheckbox = this.$("#toggle-all")[0];
 
-      // Create our collection of Todos
-      this.todos = new TodoList;
+      // Create our collection of Trips
+      this.trips = new TripList;
 
-      // Setup the query for the collection to look for todos from the current user
-      this.todos.query = new Parse.Query(Todo);
-      this.todos.query.equalTo("user", Parse.User.current());
+      // Setup the query for the collection to look for trips from the current user
+      this.trips.query = new Parse.Query(Trip);
+      this.trips.query.equalTo("user", Parse.User.current());
         
-      this.todos.bind('add',     this.addOne);
-      this.todos.bind('reset',   this.addAll);
-      this.todos.bind('all',     this.render);
+      this.trips.bind('add',     this.addOne);
+      this.trips.bind('reset',   this.addAll);
+      this.trips.bind('all',     this.render);
 
-      // Fetch all the todo items for this user
-      this.todos.fetch();
+      // Fetch all the trips for this user
+      this.trips.fetch();
 
       state.on("change", this.filter, this);
     },
@@ -200,11 +198,11 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
     // Re-rendering the App just means refreshing the statistics -- the rest
     // of the app doesn't change.
     render: function() {
-      var done = this.todos.done().length;
-      var remaining = this.todos.remaining().length;
+      var done = this.trips.done().length;
+      var remaining = this.trips.remaining().length;
 
-      this.$('#todo-stats').html(this.statsTemplate({
-        total:      this.todos.length,
+      this.$('#trip-stats').html(this.statsTemplate({
+        total:      this.trips.length,
         done:       done,
         remaining:  remaining
       }));
@@ -242,34 +240,34 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
       this.addAll();
     },
 
-    // Add a single todo item to the list by creating a view for it, and
+    // Add a single trip item to the list by creating a view for it, and
     // appending its element to the `<ul>`.
-    addOne: function(todo) {
-      var view = new TodoView({model: todo});
-      this.$("#todo-list").append(view.render().el);
+    addOne: function(trip) {
+      var view = new TripView({model: trip});
+      this.$("#trip-list").append(view.render().el);
     },
 
-    // Add all items in the Todos collection at once.
+    // Add all items in the trips collection at once.
     addAll: function(collection, filter) {
-      this.$("#todo-list").html("");
-      this.todos.each(this.addOne);
+      this.$("#trip-list").html("");
+      this.trips.each(this.addOne);
     },
 
-    // Only adds some todos, based on a filtering function that is passed in
+    // Only adds some trips, based on a filtering function that is passed in
     addSome: function(filter) {
       var self = this;
-      this.$("#todo-list").html("");
-      this.todos.chain().filter(filter).each(function(item) { self.addOne(item) });
+      this.$("#trip-list").html("");
+      this.trips.chain().filter(filter).each(function(item) { self.addOne(item) });
     },
 
-    // If you hit return in the main input field, create new Todo model
+    // If you hit return in the main input field, create new Trip model
     createOnEnter: function(e) {
       var self = this;
       if (e.keyCode != 13) return;
 
-      this.todos.create({
-        content: this.input.val(),
-        order:   this.todos.nextOrder(),
+      this.trips.create({
+        tripName: this.input.val(),
+        order:   this.trips.nextOrder(),
         done:    false,
         user:    Parse.User.current(),
         ACL:     new Parse.ACL(Parse.User.current())
@@ -279,15 +277,15 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
       this.resetFilters();
     },
 
-    // Clear all done todo items, destroying their models.
+    // Clear all done trips, destroying their models.
     clearCompleted: function() {
-      _.each(this.todos.done(), function(todo){ todo.destroy(); });
+      _.each(this.trips.done(), function(trip){ trip.destroy(); });
       return false;
     },
 
     toggleAllComplete: function () {
       var done = this.allCheckbox.checked;
-      this.todos.each(function (todo) { todo.save({'done': done}); });
+      this.trips.each(function (trip) { trip.save({'done': done}); });
     }
   });
 
@@ -311,7 +309,7 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
       
       Parse.User.logIn(username, password, {
         success: function(user) {
-          new ManageTodosView();
+          new ManageTripView();
           self.undelegateEvents();
           delete self;
         },
@@ -334,7 +332,7 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
       
       Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
         success: function(user) {
-          new ManageTodosView();
+          new ManageTripView();
           self.undelegateEvents();
           delete self;
         },
@@ -360,7 +358,7 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
   var AppView = Parse.View.extend({
     // Instead of generating a new element, bind to the existing skeleton of
     // the App already present in the HTML.
-    el: $("#todoapp"),
+    el: $("#packingapp"),
 
     initialize: function() {
       this.render();
@@ -368,7 +366,7 @@ Parse.initialize("CVwYAtfrYv0NSPbUYEnaHVEq7VhzB6VwBjcgRWZa", "2EZdaI4bbpYE1WSC2e
 
     render: function() {
       if (Parse.User.current()) {
-        new ManageTodosView();
+        new ManageTripView();
       } else {
         new LogInView();
       }
